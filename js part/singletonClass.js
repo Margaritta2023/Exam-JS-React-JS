6786443128ad75864c8258f7b44cda7b785d564f
Margaritta2023 <margarittamkn@gmail.com>
2024-02-03 15:24:29 +0400

    feat change constructor

--- js part/singletonClass.js
+++ js part/singletonClass.js
@@ -1,17 +1,25 @@
 class SingletonForURL {
+    
      constructor (){
-        if(!SingletonForURL._instance){
-
-            SingletonForURL._instance = this;
+        if(!SingletonForURL._instance){ 
             this._URL = "https://jsonplaceholder.typicode.com/posts";
-            return SingletonForURL._instance;
+            SingletonForURL._instance = this;     
        }
     else return SingletonForURL._instance;
     }
 
     async getSingletonForURL (){
-            const fetchedData = await fetch(SingletonForURL._instance) ;
+            let fetchedData = await fetch(SingletonForURL._instance._URL) ;
+            let data = await fetchedData.json();
+        //   console.log(data)
+            return data
     }
+
+    //   getSingletonForURL (){
+    //        return  fetch("https://jsonplaceholder.typicode.com/posts").then((responce)=> responce.json())
+    //        .then((data) => console.log(data))
+    //   }
+
 
     groupBy( key ){
 
@@ -22,3 +30,5 @@ class SingletonForURL {
 const url1 = new SingletonForURL();
 const url2 = new SingletonForURL();
 console.log(url1===url2)
+console.log(url1.getSingletonForURL())
+
