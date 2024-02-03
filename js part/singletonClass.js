8cc8c2d94c4d2b6a7ef605abb85505b73e78bb40
Margaritta2023 <margarittamkn@gmail.com>
2024-02-03 16:30:46 +0400

    install all libraries

--- js part/singletonClass.js
+++ js part/singletonClass.js
@@ -1,5 +1,5 @@
 class SingletonForURL {
-    
+
      constructor (){
         if(!SingletonForURL._instance){ 
             this._URL = "https://jsonplaceholder.typicode.com/posts";
@@ -9,7 +9,7 @@ class SingletonForURL {
     }
 
     async getSingletonForURL (){
-            let fetchedData = await fetch(SingletonForURL._instance._URL) ;
+            let fetchedData = await fetch(this._URL) ;
             let data = await fetchedData.json();
         //   console.log(data)
             return data
@@ -21,14 +21,31 @@ class SingletonForURL {
     //   }
 
 
-    groupBy( key ){
+    async groupBy(someKey){
 
+        let fetchedData = await fetch(this._URL) ;
+        let data = await fetchedData.json();
+     console.log(data)
+       
+       let res = data.filter((element) => element[someKey]);
+       console.log(res)
+       
+
+        // let res = [];
+        // let fetched = await fetch(this._URL);
+        // let data = fetched.json();
+        // console.log(data)
+        // .then((data)=>{data.filter((element) => element[someKey])})
+
+        // console.log("aa",aa)
     }
 
 }
 
 const url1 = new SingletonForURL();
-const url2 = new SingletonForURL();
-console.log(url1===url2)
-console.log(url1.getSingletonForURL())
+// const url2 = new SingletonForURL();
+// console.log(url1===url2)
+// console.log(url1.getSingletonForURL())
+
+console.log(url1.groupBy("userId"))
 
---
+++ modal/src/App.js
@@ -0,0 +1,21 @@
+import React,{useState} from "react"
+import React, {useState} from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { Button, TextField, Modal, Paper, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      const[modalIsOpen, setmodalIsOpen]  = useState (false)
+
+
+
+    </div>
+  );
+}
+
+export default App;
