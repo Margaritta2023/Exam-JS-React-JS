f2a4b36163e2fa5b6f5766a3fbad1a803ef37d28
Margaritta2023 <margarittamkn@gmail.com>
2024-02-03 17:15:37 +0400

    feat form change

--- modal/src/App.js
+++ modal/src/App.js
@@ -8,9 +8,39 @@ import { styled } from '@mui/system';
 import './App.css';
 
 function App() {
+
+  const StyledModal = styled(Modal)`
+  display: flex;
+ align-items: center;
+  justify-content: center;   
+`;
+const StyledPaper = styled(Paper)`
+position: absolute;
+width: 400px;
+padding: 20px;  
+`;
+ const [isModalOpen, setIsModalOpen] = useState(false);
+
+ 
+const openModal = () => {
+ setIsModalOpen(true);
+};
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Required'),
+    name: Yup.string().required('Required'),
+    surName: Yup.string().required('Required'),
+  });
+
+
   return (
     <div className="App">
-      const[modalIsOpen, setmodalIsOpen]  = useState (false)
+    
 
 
 
