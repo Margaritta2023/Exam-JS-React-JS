28ccfd5d7afbd225a3fa6e5dd3c45893a8df8035
Margaritta2023 <margarittamkn@gmail.com>
2024-02-03 17:47:40 +0400

    fixed bugs

--- modal/src/App.js
+++ modal/src/App.js
@@ -1,30 +1,15 @@
-import React,{useState} from "react"
 import React, {useState} from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
-import { Button, TextField, Modal, Paper, Typography } from '@mui/material';
-import { styled } from '@mui/system';
 import './App.css';
 
-function App() {
-
-  const StyledModal = styled(Modal)`
-  display: flex;
- align-items: center;
-  justify-content: center;   
-`;
-const StyledPaper = styled(Paper)`
-position: absolute;
-width: 400px;
-padding: 20px;  
-`;
- const [isModalOpen, setIsModalOpen] = useState(false);
-
- 
-const openModal = () => {
- setIsModalOpen(true);
-};
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
   const closeModal = () => {
     setIsModalOpen(false);
@@ -32,20 +17,94 @@ const openModal = () => {
 
 
   const validationSchema = Yup.object({
-    email: Yup.string().email('Invalid email address').required('Required'),
-    name: Yup.string().required('Required'),
-    surName: Yup.string().required('Required'),
+    text: Yup.string().email('Invalid email address').required('Required'),
+    textarea: Yup.string().required('Required'),
+    select: Yup.string().required('Required'),
   });
 
+  const initialValues = { text: '', textarea: '' , select: ""};
 
+  const onSubmit = (values) => {
+    axios.post('http://localhost:3001/users', values)
+      .then((response) => {
+        alert("sucsess saving to server")
+        console.log(response.data)})
+      .catch((error) => {
+        alert('Error saving to server', error)
+        console.log('Error saving to server', error);
+      })
+      .finally(() => {
+        closeModal();
+      
+        
+      });
+  };
+
+  
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  const { values, handleChange, handleSubmit, errors, touched } = formik;
+
+ 
   return (
     <div className="App">
-    
+      <h1>Exam React JS part</h1>
+      <button type ="button" onClick={openModal}>
+        Open Modal
+      </button>
 
+      { isModalOpen &&<div open={isModalOpen} onClose={closeModal}>
+       <form onSubmit={handleSubmit}>
+      
+     <input  type="text"  
+      name="text"
+      value={values.text} 
+      onChange={handleChange}
+      error={!!(errors.textarea && touched.text)}
+      
+       /><br/>
 
+      <textarea type="textarea"  
+      name="textarea"
+      value={values.textarea} 
+      onChange={handleChange}
+      error={!!(errors.textarea && touched.textarea)}
+     >message</textarea> <br/> 
+   <select id="tasks" value = "">
 
+    <option value="texthere 1">hello</option>
+    <option value="texthere 2">goodbye</option>
+    <option value="texthere 3">bye</option>
+  </select><br/> 
+
+ 
+
+< button type ="Submit" >Submit</button>
+<button onClick={closeModal}>Close</button>
+
+ 
+</form>
+      </div> }
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
+//             type="text"
+//             name="surName"
+//             value={values.surName}
+//             onChange={handleChange}
+//             error={!!(errors.surName && touched.surName)}
+//             helperText={errors.surName && touched.surName && errors.surName}
+//             fullWidth
+//             margin="normal"
